ef1b6c3040165b6212411c47971b906456607c3b
Eddie Rusu <rusu1@llnl.gov>
2021-05-05 15:02:06 -0700

    Testing debug, cur, and erase

--- tests/test_pydv.py
+++ tests/test_pydv.py
@@ -5,6 +5,7 @@ import sys
 import numpy as np
 
 PDV_ROOT = "/Users/rusu1/pydv"
+TEST_ROOT = os.path.join("/Users/rusu1/pydv", "tests")
 
 sys.path.append(os.path.join(PDV_ROOT, "pydv"))
 import pdv
@@ -14,7 +15,7 @@ main.app = pdv.QApplication()
 main.plotter = pdv.pdvplot.Plotter(main)
 
 def test_read():
-    main.do_read(os.path.join(PDV_ROOT, 'tests/testData.txt'))
+    main.do_read(os.path.join(TEST_ROOT, 'testData.txt'))
     assert len(main.curvelist) == 2
     assert main.curvelist[0].name == 'darkness'
     np.testing.assert_array_equal(main.curvelist[0].x, np.array([0, 1, 2, 3, 4]))
@@ -23,6 +24,33 @@ def test_read():
     np.testing.assert_array_equal(main.curvelist[1].x, np.array([0, 1, 2, 3, 4]))
     np.testing.assert_array_equal(main.curvelist[1].y, np.array([5, 4, 2.5, 2.1, 2]))
 
-test_read()
+def test_debug():
+    main.do_debug('on')
+    assert main.debug
+    main.do_debug('0')
+    assert not main.debug
+
+def test_cur():
+    main.do_cur('1 2')
+    assert len(main.plotlist) == 2
+    assert main.plotlist[0].name == 'darkness'
+    np.testing.assert_array_equal(main.plotlist[0].x, np.array([0, 1, 2, 3, 4]))
+    np.testing.assert_array_equal(main.plotlist[0].y, np.array([0, 1, 4, 9, 16]))
+    assert main.plotlist[1].name == 'lightness'
+    np.testing.assert_array_equal(main.plotlist[1].x, np.array([0, 1, 2, 3, 4]))
+    np.testing.assert_array_equal(main.plotlist[1].y, np.array([5, 4, 2.5, 2.1, 2]))
+    assert main.plotedit
+
+def test_erase():
+    main.do_erase('')
+    assert not main.plotlist
+    assert not main.usertexts
+    assert main.plotedit
+
+main.app.quit()
+
+# test_read()
+# test_cur()
+
 
 x = []
