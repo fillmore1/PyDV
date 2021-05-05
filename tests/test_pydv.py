92869f28fc2eef65411e420647460c6ebafc8e91
Eddie Rusu <rusu1@llnl.gov>
2021-05-05 15:19:22 -0700

    a few more tests

--- tests/test_pydv.py
+++ tests/test_pydv.py
@@ -41,16 +41,38 @@ def test_cur():
     np.testing.assert_array_equal(main.plotlist[1].y, np.array([5, 4, 2.5, 2.1, 2]))
     assert main.plotedit
 
-def test_erase():
-    main.do_erase('')
-    assert not main.plotlist
-    assert not main.usertexts
+def test_L1():
+    main.do_L1('a b')
+    assert len(main.plotlist) == 3
+    assert main.plotlist[2].name == 'L1 of A and B'
+    np.testing.assert_array_equal(main.plotlist[2].x, np.array([0, 1, 2, 3, 4]))
+    np.testing.assert_array_equal(main.plotlist[2].y, np.array([0, 4, 6.25, 10.45, 20.9]))
+
+def test_L2():
+    main.do_L2('a b 3.0 5.5')
+    assert len(main.plotlist) == 4
+    assert main.plotlist[3].name == 'L2 of A and B'
+    np.testing.assert_array_equal(main.plotlist[3].x, np.array([3, 4]))
+    np.testing.assert_allclose(main.plotlist[3].y, np.array([0, 11.03653025]))
+
+def test_delete():
+    main.do_del('c d')
+    assert len(main.plotlist) == 2
     assert main.plotedit
 
-main.app.quit()
+# def test_erase():
+#     main.do_erase('')
+#     assert not main.plotlist
+#     assert not main.usertexts
+#     assert main.plotedit
+
+# main.app.quit()
 
 # test_read()
+# test_debug()
 # test_cur()
+# test_L1()
+# test_L2()
 
 
 x = []
